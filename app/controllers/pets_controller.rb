f2464cadd557011e0bf74bb9c274ef0963e11272
PJC-1 <jumpzine@gmail.com>
2016-05-30 14:56:29 -0700

    added new pet method and view

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,10 @@ class PetsController < ApplicationController
     user_id = params[:user_id]
     @user = User.find_by(id: user_id)
   end
+
+  def new
+    @pet = Pet.new
+    user_id = params[:user_id]
+    @user = User.find_by(id: user_id)
+  end
 end
