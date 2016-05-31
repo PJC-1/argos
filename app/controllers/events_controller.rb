905eda816ff02da3808f51ec92c96c395721b47d
PJC-1 <jumpzine@gmail.com>
2016-05-30 19:16:54 -0700

    event model ctrl view routes check

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,68 @@
+class EventsController < ApplicationController
+  def index
+    user_id = params[:user_id]
+    @user = User.find_by(id: user_id)
+  end
+
+  def new
+    @event = Event.new
+    user_id = params[:user_id]
+    @user = User.find_by(id: user_id)
+  end
+
+  def create
+   user = User.find(params[:user_id])
+   new_event = Event.new(event_params)
+   if new_event.save
+     user.events << new_event
+     redirect_to user_event_path(user, new_event)
+   else
+     flash[:error] = new_event.errors.full_messages.join(", ")
+     redirect_to new_user_event_path(user)
+   end
+  end
+
+  def show
+    event_id = params[:id]
+    @event = Event.find_by(id: event_id)
+    user_id = params[:user_id]
+    @user = User.find_by(id: user_id)
+  end
+
+  def edit
+    event_id = params[:id]
+    @event = Event.find_by(id: event_id)
+    user_id = params[:user_id]
+    @user = User.find_by(id: user_id)
+  end
+
+  def update
+    user_id = params[:user_id]
+    user = User.find_by(id: user_id)
+    event_id = params[:id]
+    event = Event.find_by(id: event_id)
+
+    if event.update(event_params)
+      flash[:notice] = "Updated successfully."
+      redirect_to user_event_path(user, event)
+    else
+      flash[:error] = event.errors.full_messages.join(", ")
+      redirect_to edit_user_event_path(user, event)
+    end
+  end
+
+  def destroy
+    event_id = params[:id]
+    event = Event.find_by(id: event_id)
+    event.delete
+
+    user_id = params[:user_id]
+    user = User.find_by(id: user_id)
+    redirect_to user_events_path(user)
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :published_on, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,27 +18,41 @@ Rails.application.routes.draw do
 end
 
 
+# PETS
+# user_pets GET    /users/:user_id/pets(.:format)            pets#index
+#      POST   /users/:user_id/pets(.:format)            pets#create
+# new_user_pet GET    /users/:user_id/pets/new(.:format)        pets#new
+# edit_user_pet GET    /users/:user_id/pets/:id/edit(.:format)   pets#edit
+# user_pet GET    /users/:user_id/pets/:id(.:format)        pets#show
+#      PATCH  /users/:user_id/pets/:id(.:format)        pets#update
+#      PUT    /users/:user_id/pets/:id(.:format)        pets#update
+#      DELETE /users/:user_id/pets/:id(.:format)        pets#destroy
+
+# USERS
+# root GET    /                                         users#index
+# users GET    /users(.:format)                          users#index
+#      POST   /users(.:format)                          users#create
+# new_user GET    /users/new(.:format)                      users#new
+# edit_user GET    /users/:id/edit(.:format)                 users#edit
+# user GET    /users/:id(.:format)                      users#show
+#      PATCH  /users/:id(.:format)                      users#update
+#      PUT    /users/:id(.:format)                      users#update
+#      DELETE /users/:id(.:format)                      users#destroy
 
 
-# Prefix Verb   URI Pattern                             Controller#Action
-#   root GET    /                                       users#index
-# user_pets GET    /users/:user_id/pets(.:format)          pets#index
-#        POST   /users/:user_id/pets(.:format)          pets#create
-# new_user_pet GET    /users/:user_id/pets/new(.:format)      pets#new
-# edit_user_pet GET    /users/:user_id/pets/:id/edit(.:format) pets#edit
-# user_pet GET    /users/:user_id/pets/:id(.:format)      pets#show
-#        PATCH  /users/:user_id/pets/:id(.:format)      pets#update
-#        PUT    /users/:user_id/pets/:id(.:format)      pets#update
-#        DELETE /users/:user_id/pets/:id(.:format)      pets#destroy
-#  users GET    /users(.:format)                        users#index
-#        POST   /users(.:format)                        users#create
-# new_user GET    /users/new(.:format)                    users#new
-# edit_user GET    /users/:id/edit(.:format)               users#edit
-#   user GET    /users/:id(.:format)                    users#show
-#        PATCH  /users/:id(.:format)                    users#update
-#        PUT    /users/:id(.:format)                    users#update
-#        DELETE /users/:id(.:format)                    users#destroy
-# signup GET    /signup(.:format)                       users#new
-#  login GET    /login(.:format)                        sessions#new
-#        POST   /login(.:format)                        sessions#create
-# logout GET    /logout(.:format)                       sessions#destroy
+
+# EVENTS
+# user_events GET    /users/:user_id/events(.:format)          events#index
+#      POST   /users/:user_id/events(.:format)          events#create
+# new_user_event GET    /users/:user_id/events/new(.:format)      events#new
+# edit_user_event GET    /users/:user_id/events/:id/edit(.:format) events#edit
+# user_event GET    /users/:user_id/events/:id(.:format)      events#show
+#      PATCH  /users/:user_id/events/:id(.:format)      events#update
+#      PUT    /users/:user_id/events/:id(.:format)      events#update
+#      DELETE /users/:user_id/events/:id(.:format)      events#destroy
+
+# AUTH
+# signup GET    /signup(.:format)                         users#new
+# login GET    /login(.:format)                          sessions#new
+#      POST   /login(.:format)                          sessions#create
+# logout GET    /logout(.:format)                         sessions#destroy
