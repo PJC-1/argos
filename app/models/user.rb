3e9c22921615eb81716b0806e03dfb2f45d8ab9e
PJC-1 <jumpzine@gmail.com>
2016-06-09 15:45:40 -0700

    added some user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ActiveRecord::Base
   has_many :events, dependent: :destroy
   has_many :pets, dependent: :destroy
 
+  validates :email, presence: true, uniqueness: true, length: { minimum: 6 }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates :first_name, :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, :on => :create
+
+
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
