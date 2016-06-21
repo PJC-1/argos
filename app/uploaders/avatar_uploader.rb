2569cbd19b753770f48a3a3a5e4bc15592bcc74f
PJC-1 <jumpzine@gmail.com>
2016-06-21 15:37:05 -0700

    changed img resize size when upload

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,7 +9,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   version :display do
     process :eager => true
-    process :resize_to_fill => [350, 400, :north]
+    process :resize_to_fill => [300, 350, :north]
   end
 
   version :thumbnail do
