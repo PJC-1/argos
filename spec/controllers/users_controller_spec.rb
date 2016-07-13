35dab3066106247f1fcd41515c6e48f16549ac10
PJC-1 <jumpzine@gmail.com>
2016-07-13 16:05:25 -0700

    added some tests to user ctrl create show edit

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -70,14 +70,99 @@ RSpec.describe UsersController, type: :controller do
       let(:user) { FactoryGirl.create(:user) }
       let(:current_user) { user }
       before do
+        login(user)
         get :show, id: user.id
       end
-
       it "assigns @user" do
         expect(assigns(:user)).to eq(user)
       end
       it "compares current_user to not equal nil" do
         expect(current_user).to be_present
+      end
+      it "checks current_user to not be nil" do
+        expect(current_user).to_not eq(nil)
+      end
+      it "compares current_user to user.id" do
+        expect(current_user.id).to eq(user.id)
+      end
+      it "renders show view"  do
+        expect(response).to render_template(:show)
+      end
+    end
+    context "failed validations" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        get :show, id: user.id
+      end
+      it "adds a flash error message" do
+        expect(flash[:error]).to be_present
+      end
+      it "checks for if user is logged in is not true" do
+        expect(login(user)).to_not eq(true)
+      end
+      it "should redirect to the 'root path'" do
+        expect(response.status).to be(302)
+        expect(response).to redirect_to(users_path)
+      end
+    end
+  end
+  describe "#edit" do
+    context "on success" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        login(user)
+        get :edit, id: user.id
+      end
+      it "assigns @user" do
+        expect(assigns(:user)).to eq(user)
+      end
+      it "renders the edit template" do
+        expect(response).to render_template(:edit)
+      end
+    end
+    context "failed vaildations" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        get :edit, id: user.id
+      end
+      it "adds a flash error message" do
+        expect(flash[:error]).to be_present
+      end
+      it "checks for if user is logged in is not true" do
+        expect(login(user)).to_not eq(true)
+      end
+      it "should redirect to the 'root path'" do
+        expect(response.status).to be(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+  describe "#update" do
+    let(:user) { FactoryGirl.create(:user) }
+    context "on success" do
+      let(:new_first_name) { FFaker::Name.first_name }
+      let(:new_last_name) { FFaker::Name.last_name }
+      let(:new_password) { "abcdef" }
+      let(:new_email) { FFaker::Internet.email }
+      before do
+        login(user)
+        put :update, id: user.id, user: {
+          first_name: new_first_name,
+          last_name: new_last_name,
+          password: new_password,
+          email: new_email
+        }
+        user.reload
+      end
+      it "updates the user in the database" do
+        expect(user.first_name).to eq(new_first_name)
+        expect(user.last_name).to eq(new_last_name)
+        expect(user.password).to_not eq(new_password)
+        expect(user.email).to eq(new_email)
+      end
+      it "redirects to 'product_path'" do
+        expect(response.status).to be(302)
+        expect(response.location).to match(/\/users\/\d+/)
       end
     end
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,4 @@
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -20,8 +21,8 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+include SpecTestHelper
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
